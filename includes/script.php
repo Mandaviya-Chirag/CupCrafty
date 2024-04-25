79681be64277c8265ae439b613012b9d583f8b0f
Mandaviya-Chirag <cmmandaviya22@gmail.com>
2024-04-25 23:28:32 +0530

    pages updated

---
+++ includes/script.php
@@ -0,0 +1,11 @@
+<script src="<?= urlOf('assets/vendors/js/vendor.bundle.base.js')?>"></script>
+<script src="<?= urlOf('assets/vendors/chart.js/Chart.min.js')?>"></script>
+<script src="<?= urlOf('assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js')?>"></script>
+<script src="<?= urlOf('assets/vendors/progressbar.js/progressbar.min.js')?>"></script>
+<script src="<?= urlOf('assets/js/off-canvas.js')?>"></script>
+<script src="<?= urlOf('assets/js/hoverable-collapse.js')?>"></script>
+<script src="<?= urlOf('assets/js/template.js')?>"></script>
+<script src="<?= urlOf('assets/js/settings.js')?>"></script>
+<script src="<?= urlOf('assets/js/todolist.js')?>"></script>
+<script src="<?= urlOf('assets/js/dashboard.js')?>"></script>
+<script src="<?= urlOf('assets/js/Chart.roundedBarCharts.js')?>"></script>
