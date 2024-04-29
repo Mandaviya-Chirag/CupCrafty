a3ba1dc118ba85a4c61b6686324a0ac20d655218
Mandaviya-Chirag <cmmandaviya22@gmail.com>
2024-04-29 09:06:06 +0530

    sidebar updated

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,105 +2,82 @@
     <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <ul class="nav">
             <li class="nav-item">
-                <a class="nav-link" href="<?= urlOf('assets/index.html')?>">
+                <a class="nav-link" href="<?= urlOf('') ?>">
                     <i class="mdi mdi-grid-large menu-icon"></i>
                     <span class="menu-title">Dashboard</span>
                 </a>
             </li>
-            <li class="nav-item nav-category">UI Elements</li>
             <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false"
-                    aria-controls="ui-basic">
-                    <i class="menu-icon mdi mdi-floor-plan"></i>
-                    <span class="menu-title">UI Elements</span>
-                    <i class="menu-arrow"></i>
+                <a class="nav-link" href="<?= urlOf('pages/branchdetails') ?>">
+                    <i class="mdi mdi-grid-large menu-icon"></i>
+                    <span class="menu-title">BranchDetails</span>
                 </a>
-                <div class="collapse" id="ui-basic">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link"
-                                href="<?= urlOf('assets/pages/ui-features/buttons.html')?>">Buttons</a>
-                        </li>
-                        <li class="nav-item"> <a class="nav-link"
-                                href="<?= urlOf('assets/pages/ui-features/dropdowns.html')?>">Dropdowns</a></li>
-                        <li class="nav-item"> <a class="nav-link"
-                                href="<?=urlOf('assets/pages/ui-features/typography.html')?>">Typography</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item nav-category">Forms and Datas</li>
-            <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#form-elements" aria-expanded="false"
-                    aria-controls="form-elements">
-                    <i class="menu-icon mdi mdi-card-text-outline"></i>
-                    <span class="menu-title">Form elements</span>
-                    <i class="menu-arrow"></i>
-                </a>
-                <div class="collapse" id="form-elements">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"><a class="nav-link" href="<?urlOf('assets/pages/forms/basic_elements.html')?>">Basic
-                                Elements</a></li>
-                    </ul>
-                </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#charts" aria-expanded="false"
-                    aria-controls="charts">
-                    <i class="menu-icon mdi mdi-chart-line"></i>
-                    <span class="menu-title">Charts</span>
-                    <i class="menu-arrow"></i>
+                <a class="nav-link" href="<?= urlOf('pages/categories') ?>">
+                    <i class="mdi mdi-grid-large menu-icon"></i>
+                    <span class="menu-title">Categories</span>
                 </a>
-                <div class="collapse" id="charts">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="<?= urlOf('assets/pages/charts/chartjs.html')?>">ChartJs</a>
-                        </li>
-                    </ul>
-                </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#tables" aria-expanded="false"
-                    aria-controls="tables">
-                    <i class="menu-icon mdi mdi-table"></i>
-                    <span class="menu-title">Tables</span>
-                    <i class="menu-arrow"></i>
+                <a class="nav-link" href="<?= urlOf('pages/city') ?>">
+                    <i class="mdi mdi-grid-large menu-icon"></i>
+                    <span class="menu-title">City</span>
                 </a>
-                <div class="collapse" id="tables">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="<?=urlOf('assets/pages/tables/basic-table.html')?>">Basic
-                                table</a></li>
-                    </ul>
-                </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
-                    <i class="menu-icon mdi mdi-layers-outline"></i>
-                    <span class="menu-title">Icons</span>
-                    <i class="menu-arrow"></i>
+                <a class="nav-link" href="<?= urlOf('pages/expense') ?>">
+                    <i class="mdi mdi-grid-large menu-icon"></i>
+                    <span class="menu-title">Expense</span>
                 </a>
-                <div class="collapse" id="icons">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="<?=urlOf('assets/pages/icons/mdi.html')?>">Mdi icons</a></li>
-                    </ul>
-                </div>
             </li>
-            <li class="nav-item nav-category">pages</li>
             <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                    <i class="menu-icon mdi mdi-account-circle-outline"></i>
-                    <span class="menu-title">User Pages</span>
-                    <i class="menu-arrow"></i>
+                <a class="nav-link" href="<?= urlOf('pages/module') ?>">
+                    <i class="mdi mdi-grid-large menu-icon"></i>
+                    <span class="menu-title">Module</span>
                 </a>
-                <div class="collapse" id="auth">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"> <a class="nav-link" href="<?= urlOf('assets/pages/samples/login.html')?>"> Login </a>
-                        </li>
-                    </ul>
-                </div>
             </li>
-            <li class="nav-item nav-category">help</li>
             <li class="nav-item">
-                <a class="nav-link" href="<?= urlOf('http://bootstrapdash.com/demo/star-admin2-free/docs/documentation.html')?>">
-                    <i class="menu-icon mdi mdi-file-document"></i>
-                    <span class="menu-title">Documentation</span>
+                <a class="nav-link" href="<?= urlOf('pages/permission') ?>">
+                    <i class="mdi mdi-grid-large menu-icon"></i>
+                    <span class="menu-title">Permission</span>
+                </a>
+            </li>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= urlOf('pages/products') ?>">
+                    <i class="mdi mdi-grid-large menu-icon"></i>
+                    <span class="menu-title">Products</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= urlOf('pages/purchase') ?>">
+                    <i class="mdi mdi-grid-large menu-icon"></i>
+                    <span class="menu-title">Purchase</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= urlOf('pages/roles') ?>">
+                    <i class="mdi mdi-grid-large menu-icon"></i>
+                    <span class="menu-title">Roles</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= urlOf('pages/sales') ?>">
+                    <i class="mdi mdi-grid-large menu-icon"></i>
+                    <span class="menu-title">Sales</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= urlOf('pages/stock') ?>">
+                    <i class="mdi mdi-grid-large menu-icon"></i>
+                    <span class="menu-title">Stock</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= urlOf('pages/users') ?>">
+                    <i class="mdi mdi-grid-large menu-icon"></i>
+                    <span class="menu-title">Users</span>
                 </a>
             </li>
         </ul>
