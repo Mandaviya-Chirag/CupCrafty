9fac50e3c4850a10aefa87fc7338f2f7c323ac3a
Mandaviya-Chirag <cmmandaviya22@gmail.com>
2024-05-05 21:29:29 +0530

    navbarfile added for login page

---
+++ includes/navbar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Star Admin2 </title>
+    <link rel="stylesheet" href="<?= urlOf('assets/vendors/feather/feather.css') ?>">
+    <link rel="stylesheet" href="<?= urlOf('assets/vendors/mdi/css/materialdesignicons.min.css') ?>">
+    <link rel="stylesheet" href="<?= urlOf('assets/vendors/ti-icons/css/themify-icons.css') ?>">
+    <link rel="stylesheet" href="<?= urlOf('assets/vendors/typicons/typicons.css') ?>">
+    <link rel="stylesheet" href="<?= urlOf('assets/vendors/simple-line-icons/css/simple-line-icons.css') ?>">
+    <link rel="stylesheet" href="<?= urlOf('assets/vendors/css/vendor.bundle.base.css') ?>">
+    <link rel="stylesheet" href="<?= urlOf('assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css') ?>">
+    <link rel="stylesheet" href="<?= urlOf('assets/js/select.dataTables.min.css') ?>">
+    <link rel="stylesheet" href="<?= urlOf('assets/css/vertical-layout-light/style.css') ?>">
+    <link rel="shortcut icon" href="<?= urlOf('assets/images/favicon.png') ?>" />
+</head>
+
+<body>
+    <div class="container-scroller">
+        <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex align-items-top flex-row">
+            <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
+            </div>
+    </div>
+    </nav>
