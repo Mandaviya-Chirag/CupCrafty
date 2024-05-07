797deeac03c3e82b8bb8c0d052ae14cd978c1774
Mandaviya-Chirag <cmmandaviya22@gmail.com>
2024-05-07 09:14:49 +0530

    branchdetails api updated!

--- api/branchdetails/delete.php
+++ api/branchdetails/delete.php
@@ -1,10 +1,17 @@
 <?php
 
 require ('../../includes/init.php');
+header('Content-Type: application/json');
+
 $Id = $_POST['Id'];
-$query = "DELETE FROM branchdetails WHERE Id = ?";
+$query = "DELETE FROM city WHERE Id = ?";
 $param = [$Id];
 $result = execute($query, $param);
-header('location:../../pages/branchdetails/index.php');
-?>
+header('location:../../pages/city/index.php');
 
+if ($result)
+    echo json_encode(["success" => true]);
+else
+    echo json_encode(["success" => false]);
+
+?>
