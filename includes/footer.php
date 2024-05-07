37628560467d4108c11a2c170a67b45982a31775
Mandaviya-Chirag <cmmandaviya22@gmail.com>
2024-05-07 09:19:23 +0530

    footer updated

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
         <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Developed by ©  <a
-                href="https://github.com/Mandaviya-Chirag/" target="_blank">Chirag Mandaviya</a>
+                href="https://github.com/Mandaviya-Chirag/" target="_blank">ChiragMandaviya</a>
         </span>
         <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">2024 © CupCrafty </span>
     </div>
