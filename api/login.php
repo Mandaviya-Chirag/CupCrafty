f9df184e31c14af84ff9fb18d61b64d54f4dcb41
Mandaviya-Chirag <cmmandaviya22@gmail.com>
2024-05-07 09:18:18 +0530

    login, logout, updatePermission  api created!

--- api/login.php
+++ api/login.php
@@ -3,17 +3,17 @@
 require '../includes/init.php';
 header("Content-type:application/json");
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+$Name = $_POST['Name'];
+$Password = $_POST['Password'];
 
-$query = "SELECT * FROM Users WHERE Username =? AND Password =?";
-$params = [$username, $password];
+$query = "SELECT * FROM Users WHERE Name =? AND Password =?";
+$params = [$Name, $Password];
 
 $result = selectOne($query, $params);
 if ($result) {
     echo json_encode(["success" => true]);
-    $_SESSION['loggedIn'] = true;
-    $_SESSION['userId'] = $result['Id'];
+    $_SESSION['LoggedIn'] = true;
+    $_SESSION['UserId'] = $result['Id'];
 } else {
     echo json_encode(["success" => false]);
 }
