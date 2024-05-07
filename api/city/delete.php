7db01abac6e18a34e9231e7f76ba4901e9b88b35
Mandaviya-Chirag <cmmandaviya22@gmail.com>
2024-05-07 09:15:32 +0530

    city api updated!

--- api/city/delete.php
+++ api/city/delete.php
@@ -1,9 +1,16 @@
 <?php
 
 require ('../../includes/init.php');
-$Id = $_GET['id'];
+header('Content-Type: application/json');
+$Id = $_POST['Id'];
 $query = "DELETE FROM city WHERE Id = ?";
 $param = [$Id];
 $result = execute($query, $param);
-header('location:../../pages/city/index.php');
+
+if ($result)
+    echo json_encode(["success" => true]);
+else
+    echo json_encode(["success" => false]);
+
+
 ?>
